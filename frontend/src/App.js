77fc55bca46a51d4a4b60081ba403aeabc196283
Taeith <nils.pernet@gmail.com>
2020-10-08 19:28:35 +0200

    add frontend/showcase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,9 @@ import './leaflet.css';
 import { Map, TileLayer } from "react-leaflet";
 
 import './App.css';
-import './footer.css';
 import './header.css';
+import './footer.css';
+import './showcase.css';
 
 function Header() {
   return (
@@ -47,6 +48,23 @@ function Footer() {
     );
 }
 
+function ShowCase() {
+  return (
+    <Container className="showcase">
+      <Row>
+        <Col>
+          <h1>Comment ça marche ?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Comment recycler mon marc de café ?</h2>
+          <p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Comment devenir recycleur ?</h2>
+          <p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
 function App() {
   return (
     <div className="app">
@@ -59,6 +77,7 @@ function App() {
         />
       </Map>
       </div>
+      <ShowCase />
       <Footer />    
     </div>
   );
