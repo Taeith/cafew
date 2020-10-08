2caa98cc06487258ac1dbed6cbe109f0c8a969dc
Taeith <nils.pernet@gmail.com>
2020-10-08 19:16:52 +0200

    add frontend/footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 
 // leaflet
 import 'leaflet/dist/leaflet.css';
 import './leaflet.css';
 import { Map, TileLayer } from "react-leaflet";
+
+import './App.css';
+import './footer.css';
+import './header.css';
 
 function Header() {
   return (
@@ -17,17 +21,35 @@ function Header() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto"></Nav>
         <Nav>
-          <Nav.Link href="#deets">Inscription</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">Connexion</Nav.Link>
+          <Nav.Link className="header-link" href="">Inscription</Nav.Link>
+          <Nav.Link className="header-link" eventKey={2} href="">Connexion</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
 
+function Footer() {
+  return (
+      <Container className="footer">
+        <Row className="footer-top">
+          <Col>
+            <span role="img" className="logo">☕</span>
+            <p className="footer-top-text">
+              Cafew, l'application de recyclage de marc de café
+            </p>
+          </Col>
+        </Row>
+        <Row className="footer-down">
+          <Col className="footer-down-text">© 2020 <a href="https://github.com/Taeith" target="_blank">Taeith</a>. All Rights Reserved</Col>
+        </Row>
+      </Container>
+    );
+}
+
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <div>
         <Map center={[48.85, 2.6]} zoom={13}>
@@ -36,7 +58,8 @@ function App() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
       </Map>
-      </div>      
+      </div>
+      <Footer />    
     </div>
   );
 }
