eb824c167de1191950d0af7d5bcdc9562fc6c080
Taeith <nils.pernet@gmail.com>
2020-10-08 19:55:53 +0200

    add frontend/signup-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Nav, Navbar, Container, Row, Col, Modal, Button, Form } from 'react-bootstrap';
 
 // leaflet
 import 'leaflet/dist/leaflet.css';
@@ -15,6 +16,17 @@ import './footer.css';
 import './showcase.css';
 
 function Header() {
+
+  // use to open and close signup modal
+  const [showSignup, setShowSignup] = useState(false);
+  const handleCloseSignup = () => setShowSignup(false);
+  const handleShowSignup = () => setShowSignup(true);
+
+  // use to open and close login modal
+  const [showLogin, setShowLogin] = useState(false);
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home"><span role="img">☕</span> Cafew</Navbar.Brand>
@@ -22,10 +34,59 @@ function Header() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto"></Nav>
         <Nav>
-          <Nav.Link className="header-link" href="">Inscription</Nav.Link>
-          <Nav.Link className="header-link" eventKey={2} href="">Connexion</Nav.Link>
+          <Nav.Link className="header-link" onClick={handleShowSignup}>Inscription</Nav.Link>
+          <Nav.Link className="header-link" onClick={handleShowLogin}>Connexion</Nav.Link>
         </Nav>
       </Navbar.Collapse>
+
+      <Modal show={showSignup} onHide={handleCloseSignup}>
+        <Modal.Header closeButton>
+          <Modal.Title>Inscription</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Adresse e-mail</Form.Label>
+              <Form.Control type="email" placeholder="" />
+              <Form.Text className="text-muted">
+                Nous ne partagerons jamais votre e-mail avec qui que ce soit.              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicText">
+              <Form.Label>Nom d'utilisateur</Form.Label>
+              <Form.Control placeholder="" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control type="password" placeholder="" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Valider
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+      <Modal show={showLogin} onHide={handleCloseLogin}>
+        <Modal.Header closeButton>
+          <Modal.Title>Connexion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicText">
+              <Form.Label>Nom d'utilisateur</Form.Label>
+              <Form.Control placeholder="" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control type="password" placeholder="" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Valider
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
     </Navbar>
   );
 }
@@ -78,7 +139,7 @@ function App() {
       </Map>
       </div>
       <ShowCase />
-      <Footer />    
+      <Footer /> 
     </div>
   );
 }
