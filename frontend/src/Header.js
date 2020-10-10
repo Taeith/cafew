2e162aac778ae32a74c9a80c742126a2a6327e46
Taeith <nils.pernet@gmail.com>
2020-10-10 14:53:51 +0200

    add frontend/responsive-header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -8,12 +8,13 @@ import './Header.css';
 
 import Signup from './Signup';
 import Login from './Login';
+import Profile from './Profile';
 
 function Brand() {
   return (
-    <Navbar.Brand href="#home">
-      <span className="brand-logo" role="img">☕</span>
-      <span className="brand-name">Caféw Carnaval</span>
+    <Navbar.Brand>
+      <img src="/coffee.png" className="brand-logo"/>
+      <span className="brand-name">Caféw</span>
     </Navbar.Brand>
   );
 }
@@ -28,6 +29,8 @@ export default class Header extends React.Component {
       setShowSignup: false,
       showLogin: false,
       setShowLogin: false,
+      showProfile: false,
+      setShowProfile: false,
       isLoggedIn: username != null,
       username: username
     };  
@@ -35,6 +38,8 @@ export default class Header extends React.Component {
     this.handleShowSignup = () => this.setState({setShowSignup: true, showSignup: true});
     this.handleCloseLogin = () => this.setState({setShowLogin: false, showLogin: false});
     this.handleShowLogin = () => this.setState({setShowLogin: true, showLogin: true});
+    this.handleCloseProfile = () => this.setState({setShowProfile: false, showProfile: false});
+    this.handleShowProfile = () => this.setState({setShowProfile: true, showProfile: true});
     this.handleAuthentification = (username) => {
       this.setState({
         isLoggedIn: true,
@@ -61,17 +66,31 @@ export default class Header extends React.Component {
             <Navbar.Text>
               Connecté en tant que : <span className="white">{ this.state.username }</span>
             </Navbar.Text>
-            <Button variant="outline-primary" className="profileButton">Mon profil</Button>
+            <Button onClick={ this.handleShowProfile } variant="outline-primary" className="profileButton">Mon profil</Button>
             <Button onClick={ this.signout }variant="outline-danger" className="signoutButton">Déconnexion</Button>
           </Navbar.Collapse>
+          <Profile
+            showProfile={ this.state.showProfile } 
+            handleCloseProfile={ this.handleCloseProfile } />
         </Navbar>
       );
     } else {
       return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
           <Brand />
-          <Nav.Link className="header-link ml-auto" onClick={ this.handleShowSignup }>Inscription</Nav.Link>
-          <Nav.Link className="header-link" onClick={ this.handleShowLogin }>Connexion</Nav.Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link 
+                className="header-link" 
+                onClick={ this.handleShowSignup }>Inscription
+              </Nav.Link>
+              <Nav.Link 
+                className="header-link" 
+                onClick={ this.handleShowLogin }>Connexion
+              </Nav.Link>
+            </Nav>            
+          </Navbar.Collapse>
           <Signup 
             showSignup={ this.state.showSignup } 
             handleCloseSignup={ this.handleCloseSignup } />
